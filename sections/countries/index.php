7f9c7f0b1f898b4d2603479bda31c73add0fb89f
Fedor Gusarov <fedor.gusarov@paygine.ru>
2026-05-06 16:40:51 +0300

    feat: adjust css

--- sections/countries/index.php
+++ sections/countries/index.php
@@ -18,37 +18,27 @@
         <div class="countries-card__media">
           <img src="sections/countries/images/korea.png" alt="Импорт авто из Кореи" loading="lazy">
           <img class="countries-card__flag" src="images/korea-flag.svg" alt="">
-        </div>
-        <div class="countries-card__body">
-          <h3 class="countries-card__title">Импорт авто из <span class="text-gradient">Кореи</span></h3>
-          <p class="countries-card__text">Оптимальный выбор по состоянию и цене: прозрачная история, небольшие пробеги и надежные модели.</p>
-
           <div class="countries-card__accent">
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M8 1.8l1.65 3.35 3.7.54-2.68 2.62.63 3.69L8 10.25 4.7 12l.63-3.69L2.65 5.69l3.7-.54L8 1.8z" stroke="currentColor" stroke-width="1.4" stroke-linejoin="round"/>
             </svg>
             <span>Самый популярный вариант</span>
           </div>
+        </div>
+        <div class="countries-card__body">
+          <div class="countries-card__intro">
+            <h3 class="countries-card__title">Импорт авто из <span class="text-gradient">Кореи</span></h3>
+            <p class="countries-card__text">Прозрачная история, небольшие пробеги и надежные модели</p>
+          </div>
 
           <ul class="countries-card__meta">
             <li class="countries-meta-item">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8 1.7l4.5 1.7v3.95c0 2.95-1.95 5.6-4.5 6.3-2.55-.7-4.5-3.35-4.5-6.3V3.4L8 1.7z" stroke="currentColor" stroke-width="1.2"/>
-              </svg>
               <span>Проверенные площадки</span>
             </li>
             <li class="countries-meta-item">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3 12.8l5-5.15 2.05 2.1L13 6.8" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M9.95 6.8H13v3.05" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
               <span>Честные аукционы</span>
             </li>
             <li class="countries-meta-item">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8 1.7l4.5 1.7v3.95c0 2.95-1.95 5.6-4.5 6.3-2.55-.7-4.5-3.35-4.5-6.3V3.4L8 1.7z" stroke="currentColor" stroke-width="1.2"/>
-                <path d="M6.1 7.95L7.3 9.2l2.6-2.65" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
               <span>Минимальные риски</span>
             </li>
           </ul>
@@ -66,40 +56,27 @@
         <div class="countries-card__media">
           <img src="sections/countries/images/china.png" alt="Импорт авто из Китая" loading="lazy">
           <img class="countries-card__flag" src="images/china-flag.svg" alt="">
-        </div>
-        <div class="countries-card__body">
-          <h3 class="countries-card__title">Импорт авто из <span class="text-gradient">Китая</span></h3>
-          <p class="countries-card__text">Новые автомобили и электромобили по выгодной цене: современные технологии и богатые комплектации.</p>
-
           <div class="countries-card__accent">
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M8.6 1.7 4.2 8h3l-.8 6.3L11.8 8h-3.1l-.1-6.3z" stroke="currentColor" stroke-width="1.2" stroke-linejoin="round"/>
             </svg>
-            <span>Современные технологии и комплектации</span>
+            <span>Современные технологии</span>
+          </div>
+        </div>
+        <div class="countries-card__body">
+          <div class="countries-card__intro">
+            <h3 class="countries-card__title">Импорт авто из <span class="text-gradient">Китая</span></h3>
+            <p class="countries-card__text">Современные технологии и богатые комплектации</p>
           </div>
 
           <ul class="countries-card__meta">
             <li class="countries-meta-item">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="2.2" y="3.1" width="11.6" height="8.1" rx="1.5" stroke="currentColor" stroke-width="1.2"/>
-                <path d="M4 11.2v1.6m8-1.6v1.6M3.4 12.8h9.2" stroke="currentColor" stroke-width="1.2" stroke-linecap="round"/>
-              </svg>
               <span>Новые авто и электромобили</span>
             </li>
             <li class="countries-meta-item">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="8" cy="8" r="5.8" stroke="currentColor" stroke-width="1.2"/>
-                <path d="M8 5.1v5.8M5.1 8h5.8" stroke="currentColor" stroke-width="1.2" stroke-linecap="round"/>
-              </svg>
               <span>Выгодные цены</span>
             </li>
             <li class="countries-meta-item">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="2.2" y="3.5" width="8.4" height="7.6" rx="1.3" stroke="currentColor" stroke-width="1.2"/>
-                <path d="M10.6 6.1h2.6l.6 1.3v3.7h-1.3" stroke="currentColor" stroke-width="1.2" stroke-linecap="round"/>
-                <circle cx="4.4" cy="11.4" r="1" stroke="currentColor" stroke-width="1.2"/>
-                <circle cx="11.8" cy="11.4" r="1" stroke="currentColor" stroke-width="1.2"/>
-              </svg>
               <span>Быстрая доставка</span>
             </li>
           </ul>
@@ -117,38 +94,27 @@
         <div class="countries-card__media">
           <img src="sections/countries/images/usa.png" alt="Импорт авто из США" loading="lazy">
           <img class="countries-card__flag" src="images/usa-flag.svg" alt="">
-        </div>
-        <div class="countries-card__body">
-          <h3 class="countries-card__title">Импорт авто из <span class="text-gradient">США</span></h3>
-          <p class="countries-card__text">Автомобили с аукционов с максимальной экономией: большой выбор моделей и прозрачные условия покупки.</p>
-
           <div class="countries-card__accent">
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M4.1 5.4c0-1.7 1.4-3.1 3.1-3.1h1.5c1.7 0 3.1 1.4 3.1 3.1S10.4 8.5 8.7 8.5H7.2c-1.7 0-3.1 1.4-3.1 3.1s1.4 3.1 3.1 3.1h1.5c1.7 0 3.1-1.4 3.1-3.1" stroke="currentColor" stroke-width="1.2" stroke-linecap="round"/>
             </svg>
             <span>Максимальная экономия</span>
           </div>
+        </div>
+        <div class="countries-card__body">
+          <div class="countries-card__intro">
+            <h3 class="countries-card__title">Импорт авто из <span class="text-gradient">США</span></h3>
+            <p class="countries-card__text">Большой выбор моделей и прозрачные условия покупки</p>
+          </div>
 
           <ul class="countries-card__meta">
             <li class="countries-meta-item">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="2.1" y="3.1" width="11.8" height="8.3" rx="1.5" stroke="currentColor" stroke-width="1.2"/>
-                <path d="M3.8 6h8.4M5.1 8h2.3M8.6 8h2.3" stroke="currentColor" stroke-width="1.2" stroke-linecap="round"/>
-              </svg>
               <span>Аукционы США Copart / IAAI</span>
             </li>
             <li class="countries-meta-item">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="7" cy="7" r="4.6" stroke="currentColor" stroke-width="1.2"/>
-                <path d="m10.6 10.6 2.7 2.7" stroke="currentColor" stroke-width="1.2" stroke-linecap="round"/>
-              </svg>
               <span>Большой выбор</span>
             </li>
             <li class="countries-meta-item">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8 2.3 13.2 12h-10.4L8 2.3z" stroke="currentColor" stroke-width="1.2" stroke-linejoin="round"/>
-                <path d="M8 6.1v3.1M8 11.1h.01" stroke="currentColor" stroke-width="1.2" stroke-linecap="round"/>
-              </svg>
               <span>Существенная экономия</span>
             </li>
           </ul>
